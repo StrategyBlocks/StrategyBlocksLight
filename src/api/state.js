04c6d24bbbf1106a4de2f3102063ca5d1f149c3e
Glenn <glenn.murphy@strategyblocks.com>
2013-02-22 14:23:11 +1300

    .

--- src/api/state.js
+++ src/api/state.js
@@ -216,6 +216,10 @@ define(['../globals'], function(sb) {
 	};
 	
 	state.subscribe = function(type, cb) {
+		if(!sb.ext.isFunc(cb)) {
+			throw "SB_LIGHT:State -- subscribing to an event with an invalid function.";
+		}
+		
 		//console.log(sb.version);
 		sb.ext.debug("subscribing to: ", type);
 		state.subscriptions[type] = state.subscriptions[type] || [];
