e265543e8bf953f1f3b8158a0645b9359d8bf839
Glenn Murphy <glenn.murphy@strategyblocks.com>
2013-12-06 09:06:13 +1300

    block children started

--- src/sb_light/utils/consts.js
+++ src/sb_light/utils/consts.js
@@ -28,6 +28,26 @@ define(['sb_light/globals'], function(sb) {
 		}
 		return 	types;
 	};
+
+	consts.blockRollupTypes = function() { 
+		return [
+			{key:"progressRollupDefault", 	name:"Default", 	value: -1},	//-1
+			{key:"progressRollupCustom", 	name:"Custom", 		value: 0},	//0
+			{key:"progressRollupDuration", 	name:"Duration", 	value: 0},	//1
+			{key:"progressRollupEffort", 	name:"Effort", 		value: 0},	//2
+			{key:"progressRollupPriority", 	name:"Priority", 	value: 0}	//3
+		]
+	};
+	
+	consts.blockLinkRollupTypes = function() { 
+		return [
+			{key:"progressLinkRollupDefault", 	name:"Default", 	value: -1},	//-1
+			{key:"progressLinkRollupCustom", 	name:"None", 		value: 0},	//0
+			{key:"progressLinkRollupDuration", 	name:"Eliminate", 	value: 1},	//1
+			{key:"progressLinkRollupEffort", 	name:"Normal", 		value: 2}	//2
+		]
+	};
+	
 	
 	
 	return consts;
