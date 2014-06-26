f5725dcba1c95e32566ce38092ce6df95fede66b
Glenn Murphy <glenn.murphy@strategyblocks.com>
2014-06-27 11:49:01 +1200

    Request...fix problems

--- src/sb_light/api/urls.js
+++ src/sb_light/api/urls.js
@@ -186,7 +186,7 @@ define(['sb_light/globals'], function(sb) {
 		ADMIN_RECALC_HEALTH:		{url: "/admin/recalculate_health/%id%.js", 				post:false, normalParams:true, deleteId:true},
 		ADMIN_REGOS:				{url: "/admin/rego_tokens.js", 							post:false, normalParams:true},
 		ADMIN_REPORTS:				{url: "/admin/report.csv", 								post:false, normalParams:true, dataType:"text"},
-		ADMIN_REQUESTS:				{url: "/admin/current_requests.js", 					post:false, normalParams:true},
+		ADMIN_REQUESTS:				{url: "/admin/current_requests.js", 					post:false, normalParams:true, company:"admin_company_id", user:"admin_user_id"},
 		ADMIN_RESET_PASSWORD:		{url: "/admin/generate_new_password.js", 				post:true, normalParams:true},
 		ADMIN_SESSIONS:				{url: "/admin/current_sessions.js", 					post:false, normalParams:true},
 		ADMIN_SETTINGS:				{url: "/admin/settings.js", 							post:false},
