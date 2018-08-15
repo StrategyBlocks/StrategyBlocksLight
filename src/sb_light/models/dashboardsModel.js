ee5c9bcd45d445482659b6104b9e707c12fb932f
Glenn Murphy <glenn.murphy@strategyblocks.com>
2018-08-15 16:55:33 +1200

    Dashboard widget / layering issues, and widget edit refactor for mixins

--- src/sb_light/models/dashboardsModel.js
+++ src/sb_light/models/dashboardsModel.js
@@ -26,7 +26,32 @@ define(['sb_light/models/_abstractModel','sb_light/globals'], function( _Model, 
 				d.is_owner = u.id == d.owner_id;
 				E.each(d.widgets, function(w) {
 					w.id = w.id || E.unique("widget");
-				})
+				});
+
+				var normalizeDepth = E._.sortBy(
+					E.map(d.widgets, function(w) {
+						return {depth: E.first(w.depth, 0), widget: w};
+					}), 'depth'
+				);
+
+
+				//widgets pushed to the FRONT
+				var above = E._.filter(normalizeDepth, function(v) {
+					return v.depth > 0;
+				});
+				E.each(above, function(v,k) {
+					v.widget.depth = k+1;
+				});
+
+				//widgets pushed to to BACK
+				var below = E._.filter(normalizeDepth, function(v) {
+					return v.depth < 0;
+				});
+				
+				E.each(below.reverse(), function(v,k) {
+					v.widget.depth = -(k+1);
+				});
+
 			});
 
 		}		
