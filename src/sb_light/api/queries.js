6b304351b3a6cd74b5acd0d0e3df3e97f75be711
Glenn Murphy <glenn.murphy@strategyblocks.com>
2018-10-25 14:45:33 +1300

    Fix typo

--- src/sb_light/api/queries.js
+++ src/sb_light/api/queries.js
@@ -882,7 +882,7 @@ define(['sb_light/globals',
 				var sk = k+"_scale";
 
 				data[rk] = E.values(data[k], "value");
-				data[dk] = E.values(data[k], "dates", D.serverStr);
+				data[dk] = E.values(data[k], "date", D.serverStr);
 
 				if(k.match(/actuals/))  {
 					data[dk].unshift(D.date(data[dk][0], {minutes: -1}));
