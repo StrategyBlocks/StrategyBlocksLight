ed366ba602080c09dd4bc00fcd356250d970571b
Glenn Murphy <glenn.murphy@strategyblocks.com>
2018-12-07 15:34:15 +1300

    Fix key

--- src/sb_light/models/blocksModel.js
+++ src/sb_light/models/blocksModel.js
@@ -401,7 +401,7 @@ define(['sb_light/models/_abstractModel','sb_light/globals','fuse'], function( _
 							(b.manager_id == uid ? "managed" : 
 							(b.ownership_state == "watched" ? "watched" : "none") ) ),
 
-				dependencies: (b.dependencies || [])
+				dependency_ids: (b.dependency_ids || [])
 			});
 
 			E._.each(b.metrics, function(v) {
